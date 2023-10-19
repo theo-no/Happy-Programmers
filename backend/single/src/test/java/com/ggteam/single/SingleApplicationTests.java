f4526bfb75d2e9719fff44ea256b08e3289575be
정진욱 <wlsdnr214@naver.com>
2023-10-19 15:28:49 +0900

    Init: create backend project

---
+++ backend/single/src/test/java/com/ggteam/single/SingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggteam.single;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
