d922bb0b65053852dba425fbd3a31abe14a43b17
haeum <garlic820@gmail.com>
2023-10-25 09:18:53 +0900

    fix : 스프링부트 test 종속성 작성방법 변경

---
+++ backend/single/src/main/java/api/state/controller/StatController.java
@@ -0,0 +1,14 @@
+package api.state.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stat")
+@Slf4j
+public class StatController {
+
+}
