fb720121d3c2afe62584e3b197846aba80b36510
SonJunBae <thswnsqo99@gmail.com>
2023-10-30 18:01:00 +0900

    feat: 회원가입 및 로그인 구현 중

--- backend/single/src/main/java/com/ggteam/single/api/account/Role.java
+++ backend/single/src/main/java/com/ggteam/single/api/account/Role.java
@@ -1,5 +1,12 @@
 package com.ggteam.single.api.account;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    USER, ADMIN
+    USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+
+    private final String key;
 }
--- backend/single/src/main/java/com/ggteam/single/api/account/entity/Account.java
+++ backend/single/src/main/java/com/ggteam/single/api/account/entity/Account.java
@@ -15,16 +15,23 @@ import javax.persistence.*;
 public class Account {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
     private String accountId;
     private String password;
     private String nickname;
     private String language;
+
     private String refreshToken;
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "character_id", referencedColumnName = "id")
+    @JsonManagedReference
+    private Character character;
 
     public void authorizeUser() {
         this.role = Role.USER;
@@ -34,12 +41,15 @@ public class Account {
         this.password = passwordEncoder.encode(this.password);
     }
 
-    public void updateRefreshToken(String updateRefreshToken) {
-        this.refreshToken = updateRefreshToken;
+    public void updateRefreshToken(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
     }
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "character_id", referencedColumnName = "id")
-    @JsonManagedReference
-    private Character character;
+    public void updateNickname(String newNickname) {
+        this.nickname = newNickname;
+    }
+
+    public void updatePassword(String newPassword, PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(newPassword);
+    }
 }
