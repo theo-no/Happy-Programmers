0a5267ecac91e72a857b1443a31f14a5a60c76f7
정진욱 <wlsdnr214@naver.com>
2023-10-30 16:34:32 +0900

    Feat: 더미데이터 추가 및 Item 조회 기능 구현

--- backend/single/src/main/java/com/ggteam/single/api/guide/entity/Monster.java
+++ backend/single/src/main/java/com/ggteam/single/api/guide/entity/Monster.java
@@ -1,5 +1,6 @@
 package com.ggteam.single.api.guide.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Monster {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false, unique = true)
 	private String name;
 
 	private String description;
