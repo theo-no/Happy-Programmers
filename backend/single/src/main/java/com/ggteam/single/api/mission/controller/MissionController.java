7e20b7f9bc5c3a2b523df0ec528cf02dfac8f587
haeum <garlic820@gmail.com>
2023-11-01 15:27:18 +0900

    feat : 객체인식 API 스웨거 메모 추가

--- backend/single/src/main/java/com/ggteam/single/api/mission/controller/MissionController.java
+++ backend/single/src/main/java/com/ggteam/single/api/mission/controller/MissionController.java
@@ -3,6 +3,8 @@ package com.ggteam.single.api.mission.controller;
 
 import com.ggteam.single.api.mission.service.MissionService;
 import com.google.gson.JsonObject;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +12,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 
+@Tag(name ="미션", description = "게임 외부에서 수행해야하는 미션")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/game-feature")
 @Slf4j
 public class MissionController {
 
+    @Operation(summary = "AI 객체 인식, quest-item에는 인식해야 하는 아이템을 영어로 입력 ex)keyboard")
     @GetMapping("/picture/{quest-item}")
     @CrossOrigin(origins = "*")
     public boolean detectObject(@PathVariable("quest-item") String questItem, @RequestPart(name = "file") MultipartFile image){
