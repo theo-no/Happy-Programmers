e2300a40c74e2eceb6436133bfad83ceac2c4dad
차선호 <tjsgh1512@naver.com>
2023-11-01 17:32:04 +0900

    TODO: Room 테스트 해야 함 | 차선호

---
+++ android/HappyProgrammer/data/src/main/java/com/gumigames/data/mapper/ItemMapper.kt
@@ -0,0 +1,13 @@
+package com.gumigames.data.mapper
+
+import com.gumigames.data.datasource.dao.ItemBookmarkDao
+import com.gumigames.domain.model.item.ItemDto
+
+/**
+ * 로컬에서 가져온 Entity를 domain의 model로 변환
+ */
+//fun ItemBookmarkDao.toDomain(): ItemDto{
+//    return ItemDto(
+//
+//    )
+//}
