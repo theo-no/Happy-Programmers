acdc2eba77725b2572acaa616f5272396261f191
theo-no <tjsgh1512@naver.com>
2023-11-02 20:16:33 +0900

    FEAT: Bookmark Item 조회 완료 | 차선호

--- android/HappyProgrammer/presentation/src/main/java/com/gumigames/presentation/ui/bookmark/BookmarkFragment.kt
+++ android/HappyProgrammer/presentation/src/main/java/com/gumigames/presentation/ui/bookmark/BookmarkFragment.kt
@@ -6,10 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide.init
 import com.freeproject.happyprogrammers.base.BaseFragment
+import com.google.android.material.tabs.TabLayout
 import com.gumigames.presentation.R
 import com.gumigames.presentation.databinding.FragmentBookmarkBinding
+import com.gumigames.presentation.ui.dogam.item.ItemListApdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class BookmarkFragment : BaseFragment<FragmentBookmarkBinding>(
@@ -18,12 +25,71 @@ class BookmarkFragment : BaseFragment<FragmentBookmarkBinding>(
 ) {
 
     private val bookmarkViewModel: BookmarkViewModel by viewModels()
+    private lateinit var itemListAdapter: ItemListApdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bookmarkViewModel.getAllBookmarkItemsLocal()
+        init()
+        initView()
         initListener()
+        initCollect()
+        collectErrorAndToken(bookmarkViewModel)
     }
+
+    private fun init(){
+        bookmarkViewModel.getAllBookmarkItemsLocal()
+        itemListAdapter = ItemListApdapter()
+    }
+
+    private fun initView(){
+        initBookmarkRecyclerView()
+    }
+
+    private fun initBookmarkRecyclerView(){
+        binding.recyclerviewBookmark.apply {
+            adapter = itemListAdapter
+            layoutManager = GridLayoutManager(mActivity, 3)
+        }
+    }
+
     override fun initListener() {
+        binding.apply {
+            tablayoutBookmark.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    when (tab!!.position) {
+                        //아이템 조회
+                        0 -> {
+                            binding.recyclerviewBookmark.adapter = itemListAdapter
+                            bookmarkViewModel.getAllBookmarkItemsLocal()
+                            bookmarkViewModel.setCurrentTab("item")
+                        }
+                        //스킬 조회
+                        1 -> {
+                            bookmarkViewModel.setCurrentTab("skill")
+                        }
+                        //몬스터 조회
+                        2 -> {
+                            bookmarkViewModel.setCurrentTab("monster")
+                        }
+                    }
+                }
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+        }
     }
+
+    private fun initCollect(){
+        bookmarkViewModel.apply {
+            //현재 북마크 아이템 리스트 관찰
+            viewLifecycleOwner.lifecycleScope.launch {
+                currentBookmarkItemList.collectLatest {
+                    itemListAdapter.submitList(it)
+                }
+            }
+        }
+
+    }
+
 }
--- android/HappyProgrammer/presentation/src/main/java/com/gumigames/presentation/ui/bookmark/BookmarkViewModel.kt
+++ android/HappyProgrammer/presentation/src/main/java/com/gumigames/presentation/ui/bookmark/BookmarkViewModel.kt
@@ -2,9 +2,15 @@ package com.gumigames.presentation.ui.bookmark
 
 import android.util.Log
 import androidx.lifecycle.viewModelScope
+import com.gumigames.domain.model.item.ItemDto
 import com.gumigames.domain.usecase.bookmark.item.GetAllBookmarkItemsLocalUseCase
 import com.gumigames.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,9 +20,39 @@ class BookmarkViewModel @Inject constructor(
     private val getAllBookmarkItemsLocalUseCase: GetAllBookmarkItemsLocalUseCase
 ): BaseViewModel(){
 
-    fun getAllBookmarkItemsLocal(){
+    //현재 탭
+    private var _currentTab: String = "item"
+    fun setCurrentTab(tab: String){
+        _currentTab = tab
+    }
+
+
+
+    //현재 아이템 북마크 리스트
+    private var _currentBookmarkItemList = MutableSharedFlow<List<ItemDto>>()
+
+    val currentBookmarkItemList: SharedFlow<List<ItemDto>>
+        get() = _currentBookmarkItemList.asSharedFlow()
+
+    //현재 선택된 아이템
+    private var _selectedBookmarkItem = MutableStateFlow<ItemDto?>(null)
+    val selectedBookmarkItem = _selectedBookmarkItem.asStateFlow()
+    fun setSelectedBookmarkItem(item: ItemDto?){
         viewModelScope.launch {
-            Log.d(TAG, "getAllBookmarkItemsLocal result : ${getAllBookmarkItemsLocalUseCase.invoke()}")
+            _selectedBookmarkItem.emit(item)
         }
     }
+
+    //전체 아이템 조회
+    fun getAllBookmarkItemsLocal(){
+        viewModelScope.launch {
+            _currentBookmarkItemList.emit(getAllBookmarkItemsLocalUseCase.invoke())
+        }
+    }
+    //아이템 검색
+    fun getSearchItems(){
+        viewModelScope.launch {
+        }
+    }
+
 }
