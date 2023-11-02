8c1596a2b440abab774a31d8254c5cb1e0c4eab9
정진욱 <wlsdnr214@naver.com>
2023-11-02 14:24:02 +0900

    Feat: DTO에 ID값 추가

--- backend/single/src/main/java/com/ggteam/single/api/guide/entity/ItemFavorite.java
+++ backend/single/src/main/java/com/ggteam/single/api/guide/entity/ItemFavorite.java
@@ -9,18 +9,18 @@ import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@Entity
-public class ItemFavorite {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-
-	@ManyToOne
-	private Item item;
-
-	// @ManyToOne
-	// private Account account;
-}
+// @Getter
+// @NoArgsConstructor
+// // @Entity
+// public class ItemFavorite {
+//
+// 	@Id
+// 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+// 	private Integer id;
+//
+// 	@ManyToOne
+// 	private Item item;
+//
+// 	// @ManyToOne
+// 	// private Account account;
+// }
--- backend/single/src/main/java/com/ggteam/single/api/guide/entity/SkillFavorite.java
+++ backend/single/src/main/java/com/ggteam/single/api/guide/entity/SkillFavorite.java
@@ -9,18 +9,18 @@ import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@Entity
-public class SkillFavorite {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-
-	@ManyToOne
-	private Skill skill;
-
-	// @ManyToOne
-	// private Account account;
-}
+// @Getter
+// @NoArgsConstructor
+// // @Entity
+// public class SkillFavorite {
+//
+// 	@Id
+// 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+// 	private Integer id;
+//
+// 	@ManyToOne
+// 	private Skill skill;
+//
+// 	// @ManyToOne
+// 	// private Account account;
+// }
