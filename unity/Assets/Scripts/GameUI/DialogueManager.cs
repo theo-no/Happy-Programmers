8300531a1decb5fd27cca4e249f1f8b5d27ca2c3
sm9906 <suminkim0618@gmail.com>
2023-11-08 10:19:56 +0900

    feat: 대화창 닫힌 후 스페이스바 눌렀을 때 오류 해결

--- unity/Assets/Scripts/GameUI/DialogueManager.cs
+++ unity/Assets/Scripts/GameUI/DialogueManager.cs
@@ -35,6 +35,10 @@ public class DialogueManager : MonoBehaviour
     // public Animator animSprite;
     public Animator animDialogueWindow;
 
+    public bool talking = false;
+
+    private CharacterMovement thePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,6 +51,8 @@ public class DialogueManager : MonoBehaviour
 
     public void ShowDialogue(Dialogue dialogue)
     {
+        talking = true;
+
         for (int i = 0; i < dialogue.sentences.Length; i++)
         {
             listSentences.Add(dialogue.sentences[i]);
@@ -68,6 +74,7 @@ public class DialogueManager : MonoBehaviour
         listDialogueWindows.Clear();
         // animSprite.SetBool("Appear", false);
         animDialogueWindow.SetBool("Appear", false);
+        talking = false;
     }
 
     IEnumerator StartDialogueCoroutine()
@@ -116,20 +123,23 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(talking)
         {
-            count++;
-            text.text = "";
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                count++;
+                text.text = "";
 
-            if (count == listSentences.Count)
-            {
-                StopAllCoroutines();
-                ExitDialogue();
-            }
-            else
-            {
-                StopAllCoroutines();
-                StartCoroutine(StartDialogueCoroutine());
+                if (count == listSentences.Count)
+                {
+                    StopAllCoroutines();
+                    ExitDialogue();
+                }
+                else
+                {
+                    StopAllCoroutines();
+                    StartCoroutine(StartDialogueCoroutine());
+                }
             }
         }
     }
