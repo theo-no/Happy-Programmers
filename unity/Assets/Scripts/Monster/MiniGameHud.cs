566aca106c22609915a94d5115af07b0e6c5501f
haeum <garlic820@gmail.com>
2023-11-14 13:15:25 +0900

    fix : 충돌해결
    Former-commit-id: ebe3101b375f29b8a92d1cc5d334d9f3e7f08238

--- unity/Assets/Scripts/Monster/MiniGameHud.cs
+++ unity/Assets/Scripts/Monster/MiniGameHud.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MiniGameHud : MonoBehaviour
 {
-    public enum InfoType{EXP, kill, Level, Health, Time}
+    public enum InfoType{EXP, kill, Level, Health, Time, Mp, EXPText }
 
     public InfoType type;
 
-    Text myText;
+    TextMeshProUGUI myText;
     Slider mySlider;
 
     private void Awake()
     {
-        myText = GetComponent<Text>();
+        myText = GetComponent<TextMeshProUGUI>();
         mySlider = GetComponent<Slider>();
     }
 
@@ -24,14 +25,28 @@ public class MiniGameHud : MonoBehaviour
         {
             case InfoType.EXP:
                 float curExp = MiniGameManager.instance.exp;
+                float maxExp = MiniGameManager.instance.nextExp[MiniGameManager.instance.level];
+                mySlider.value = curExp / maxExp;
+                break;
+            case InfoType.EXPText:
+                myText.text = string.Format("È¹µæÇÑ °æÇèÄ¡ : {0} ", MiniGameManager.instance.exp);
                 break;
             case InfoType.Level:
+                myText.text = string.Format("{0} ·¹º§ÀÌ »ó½ÂÇß½À´Ï´Ù.", MiniGameManager.instance.level);
                 break;
             case InfoType.kill:
                 break;
             case InfoType.Time:
                 break;
             case InfoType.Health:
+                float curHealth = MiniGameManager.instance.health;
+                float maxHealth = MiniGameManager.instance.maxHealth;
+                mySlider.value = curHealth / maxHealth;
+                break;
+            case InfoType.Mp:
+                float curMP = MiniGameManager.instance.mp;
+                float maxMP = MiniGameManager.instance.maxMp;
+                mySlider.value = curMP / maxMP;
                 break;
 
 
