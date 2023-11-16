0f145c3b355d39befb42aad696f90789d95acd71
haeum <garlic820@gmail.com>
2023-11-16 14:13:36 +0900

    feat : 몬스터 처치, 경험치 상승, MP 하락 게임 종료 시 창 나오기 구현
    Former-commit-id: 494200c94f2a924b660584018645f78fe5523707

--- unity/Assets/Scripts/Monster/MiniGameHud.cs
+++ unity/Assets/Scripts/Monster/MiniGameHud.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class MiniGameHud : MonoBehaviour
 {
-    public enum InfoType{EXP, kill, Level, Health, Time, Mp, EXPText }
+    public enum InfoType{EXP, kill, Level, Health, Time, Mp }
 
     public InfoType type;
 
@@ -28,15 +28,13 @@ public class MiniGameHud : MonoBehaviour
                 float maxExp = MiniGameManager.instance.nextExp[MiniGameManager.instance.level];
                 mySlider.value = curExp / maxExp;
                 break;
-            case InfoType.EXPText:
-                myText.text = string.Format("획득한 경험치 : {0} ", MiniGameManager.instance.exp);
-                break;
             case InfoType.Level:
-                myText.text = string.Format("{0} 레벨이 상승했습니다.", MiniGameManager.instance.level);
                 break;
             case InfoType.kill:
+                myText.text = string.Format("{0} 개의 버그를 처리했습니다.", MiniGameManager.instance.kill);
                 break;
             case InfoType.Time:
+                myText.text = string.Format(" 처리 시간 : {0}", MiniGameManager.instance.gameTime);
                 break;
             case InfoType.Health:
                 float curHealth = MiniGameManager.instance.health;
